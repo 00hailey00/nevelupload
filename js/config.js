bc96536406caa16e9a3d5050d2d244a77ec8b5a1
00hailey00 <pys8611999@gmail.com>
2025-08-05 23:58:18 +0900

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,5 @@
+/* ========= GitHub 설정 ========= */
+const GH_TOKEN  = 'ghp_*****************************';  // Personal Access Token
+const GH_OWNER  = 'your-github-id';                    // 깃허브 ID
+const GH_REPO   = 'your-repo-name';                    // 저장소 이름
+const GH_BRANCH = 'main';                              // 브랜치 (기본 main)
